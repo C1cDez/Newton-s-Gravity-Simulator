6d091db68246003305da53c81b2c78aacb451ed2
C1cDez <c1cdezxxx@gmail.com>
2023-09-01 22:23:17 +0300

    Graphics updated with Volatile Image

--- src/com/cicdez/newtonsgravity/Screen.java
+++ src/com/cicdez/newtonsgravity/Screen.java
@@ -2,12 +2,13 @@ package com.cicdez.newtonsgravity;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.VolatileImage;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Screen extends JFrame implements Runnable {
-    public static final int WIDTH = 1300, HEIGHT = 700;
+    public static final int FPS = 1500;
+    public static final int WIDTH = 1800, HEIGHT = 900;
     public static final Color BACKGROUND = new Color(13, 41, 74);
     public static final double DT = 0.01;
     public static final double ZOOM = 1;
@@ -16,28 +17,34 @@ public class Screen extends JFrame implements Runnable {
 
     private final boolean enableTrajectories;
 
-    public final BufferedImage screen = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-    public final BufferedImage trajectories = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+    private VolatileImage screen;
+    private VolatileImage trajectory;
 
     public final List<PhysicalBody> physicalBodies = new ArrayList<>();
 
     public Screen(boolean enableTrajectories) {
-        super("Gravitation Simulation");
+        super("Newton's Gravitation Simulation");
         this.enableTrajectories = enableTrajectories;
         this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setVisible(true);
+        
+        this.screen = this.getGraphicsConfiguration().createCompatibleVolatileImage(WIDTH, HEIGHT);
+        this.trajectory = this.getGraphicsConfiguration().createCompatibleVolatileImage(WIDTH, HEIGHT);
     }
 
-    @Override
-    public void paint(Graphics graphics) {
+    public void render() {
+        if (screen.validate(this.getGraphicsConfiguration()) == VolatileImage.IMAGE_INCOMPATIBLE) {
+            screen = this.getGraphicsConfiguration().createCompatibleVolatileImage(WIDTH, HEIGHT);
+        }
+        if (trajectory.validate(this.getGraphicsConfiguration()) == VolatileImage.IMAGE_INCOMPATIBLE) {
+            trajectory = this.getGraphicsConfiguration().createCompatibleVolatileImage(WIDTH, HEIGHT);
+        }
         updateBodies();
-        drawBodies(screen.createGraphics(), trajectories.createGraphics());
-        graphics.drawImage(screen, 0, 0, WIDTH, HEIGHT, null);
-        if (enableTrajectories)
-            graphics.drawImage(trajectories, 0, 0, WIDTH, HEIGHT, null);
+        drawBodies(screen.createGraphics(), trajectory.createGraphics());
+        this.getGraphics().drawImage(screen, 0, 0, WIDTH, HEIGHT, null);
     }
 
     public void updateBodies() {
@@ -47,16 +54,13 @@ public class Screen extends JFrame implements Runnable {
                 double force = Mathematics.calculateForce(body, otherBody);
                 double forceX = force * (otherBody.x - body.x),
                         forceY = force * (otherBody.y - body.y);
-                updateBody(body, forceX, forceY, DT);
-                updateBody(otherBody, -forceX, -forceY, DT);
+                body.update(forceX, forceY, DT);
+                otherBody.update(-forceX, -forceY, DT);
             }
         }
     }
-    public void updateBody(PhysicalBody body, double fx, double fy, double dt) {
-        body.update(fx, fy, dt);
-    }
 
-    public void drawBodies(Graphics2D graphics, Graphics2D trajectory) {
+    public void drawBodies(Graphics2D graphics, Graphics2D trajectoryGraphics) {
         graphics.setColor(BACKGROUND);
         graphics.fillRect(0, 0, WIDTH, HEIGHT);
         for (PhysicalBody body : physicalBodies) {
@@ -65,9 +69,10 @@ public class Screen extends JFrame implements Runnable {
             graphics.setColor(body.color);
             graphics.fillOval((int) (rx - rr), (int) (ry - rr), (int) (rr * 2), (int) (rr * 2));
             if (enableTrajectories) {
-                trajectory.setColor(body.color);
-                trajectory.fillOval((int) (rx - 1.5 / ZOOM), (int) (ry - 1.5 / ZOOM),
+                trajectoryGraphics.setColor(body.color);
+                trajectoryGraphics.fillOval((int) (rx - 1.5 / ZOOM), (int) (ry - 1.5 / ZOOM),
                         3, 3);
+                graphics.drawImage(trajectory, 0, 0, WIDTH, HEIGHT, null);
             }
         }
     }
@@ -75,17 +80,24 @@ public class Screen extends JFrame implements Runnable {
     @Override
     public void run() {
         makeBodies();
+        double interval = 1e9 / FPS;
+        double nextInterval = System.nanoTime() + interval;
         while (true) {
-            this.repaint();
+            render();
+            double remaining = (nextInterval - System.nanoTime()) / 1e6;
+            if (remaining < 0) remaining = 0;
+            try {
+                Thread.sleep((long) remaining);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            nextInterval += interval;
         }
     }
 
     public void makeBodies() {
-        for (int i = 0; i < 250; i++) {
-//            if (i == 0) physicalBodies.add(new PhysicalBody(2500, 20,
-//                    CENTER_X, CENTER_Y, 0, 0));
-            physicalBodies.add(new PhysicalBody(Math.random() * 10, 5,
-                    Math.random() * WIDTH * ZOOM, Math.random() * HEIGHT * ZOOM, 0, 0));
+        for (int i = 0; i < 100; i++) {
+            physicalBodies.add(new PhysicalBody(1, 10, Math.random() * WIDTH, Math.random() * HEIGHT, 0, 0));
         }
     }
 }
